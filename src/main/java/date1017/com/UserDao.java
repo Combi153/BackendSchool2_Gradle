3445c93d3f580d3e3637b41f7b2df5003c19350c
chanminmaxscaler <jcm04@naver.com>
2022-10-18 11:38:05 +0900

    [수정] UserDao connection 메소드 분리, add 매개변수 추가, findById 메소드 추가 [추가] User [추가] UserDaoTest

--- src/main/java/date1017/com/UserDao.java
+++ src/main/java/date1017/com/UserDao.java
@@ -1,30 +1,73 @@
 package date1017.com;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import date1017.com.domain.User;
+
+import java.sql.*;
+import java.util.Map;
 
 public class UserDao {
-    public void add () throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.ch.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://ec2-3-37-29-229.ap-northeast-2.compute.amazonaws.com/likelion","root", "12345678"
-        );
-        PreparedStatement ps = conn.prepareStatement(
-                "INSERT INTO users(id, name, password) VALUES(?, ?, ?)"
-        );
-        ps.setString(1, "0");
-        ps.setString(2, "chanmin");
-        ps.setString(3, "1233");
 
-        int status = ps.executeUpdate();
-        ps.close();
-        conn.close();
+    private String dbHost;
+    private String dbUser;
+    private String dbPassword;
+
+    public UserDao() {
+        Map<String, String> env = System.getenv();
+        this.dbHost = env.get("DB_HOST");
+        this.dbUser = env.get("DB_USER");
+        this.dbPassword = env.get("DB_PASSWORD");
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        return conn;
+    }
+
+    public void add (User user) {
+
+        try{
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            int status = ps.executeUpdate();
+            ps.close();
+            conn.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement ("SELECT * FROM users WHERE id = ?");
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            rs.close();
+            ps.close();
+            conn.close();
+            return user;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        userDao.add();
+        /*User user = new User("7", "김창환", "1234");
+        userDao.add(user);*/
+        System.out.println(userDao.findById("7"));
     }
 }
